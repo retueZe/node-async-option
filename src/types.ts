075d22bd631cddbabc733fb644142ed1929706c8
retueZe <84510709+retueZe@users.noreply.github.com>
2022-09-23 03:55:19 +0300

    Add @since annotations in src/types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 import { Async, IOption, IResult } from './abstraction'
 
+/** @since v1.7.0 */
 export type OptionValue<T> = T extends Async<IOption<infer U>> ? U : never
+/** @since v1.7.0 */
 export type ResultValue<T> = T extends Async<IResult<infer U, any>> ? U : never
+/** @since v1.7.0 */
 export type ResultError<T> = T extends Async<IResult<any, infer U>> ? U : never
